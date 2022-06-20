34c9e6dc6d47bac6c092ddbda56c0b26a0798343
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-06-20 21:13:29 +0000

    perform migrations and setup superuser for admin

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
+# python3 manage.py runserver
+# python3 manage.py makemigrations --dry-run
+# python3 manage.py showmigrations
+# python3 manage.py makemigrations --dry-plan
+# python3 manage.py makemigrations
+# python3 manage.py migrate
+# python3 manage.py createsuperuser
+# username: ckz8780, pantera101
 from django.shortcuts import render
 
 # Create your views here.
