ce1d98ecc3293608e9d45a0ad65a0a455df6a5ef
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-06-20 22:44:46 +0000

    create tables in and admin and function to return the name of these itemmsems

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,17 @@
+# python3 manage.py runserver
+# python3 manage.py makemigrations --dry-run
+# python3 manage.py showmigrations
+# python3 manage.py makemigrations --dry-plan
+# python3 manage.py migrate --dry-plan
+# python3 manage.py createsuperuser
+# username: ckz8780, pantera101
 from django.db import models
 
 
 # Create your models here.
 class Item(models.Model):
-    name = models.CharField(max_lenght=50, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
