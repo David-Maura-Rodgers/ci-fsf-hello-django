43eed09f5078848215d513203e6b6428ded8734c
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-06-22 20:28:52 +0000

    create template variable {{ item }} and table in html template to render tr in the view

--- todo/views.py
+++ todo/views.py
@@ -7,9 +7,14 @@
 # python3 manage.py createsuperuser
 # username: ckz8780, pantera101
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, "todo/todo_list.html")
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, "todo/todo_list.html", context)
